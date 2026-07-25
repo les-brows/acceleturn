9b33f78514eec187744bfe7a2ac4fbb95beb7b9f
Nebulea <github@nebulea.dev>
2026-07-26 00:42:25 +0200

    UI: add sprites for tile selection and callbacks for clicking them

--- Objects/Entities.gd
+++ Objects/Entities.gd
@@ -5,7 +5,7 @@ var caseCoords: Vector2i = Vector2(0,0)
 var _level: Level = null
 
 func init_entity(level: Level):
-	_level = level	
+	_level = level
 	caseCoords = _level.getCoordinatesFromPostionAbsolute(position)
 	update_visual()
 
