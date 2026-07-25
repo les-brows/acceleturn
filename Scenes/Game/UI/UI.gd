3544ab2d694ff6462a2a329c8959362bdda1f004
Nebulea <github@nebulea.dev>
2026-07-25 16:44:43 +0200

    UI: connect the state machine from Helios to the UI buttons
    Signed-off-by: Nebulea <github@nebulea.dev>

--- Scenes/Game/UI/UI.gd
+++ Scenes/Game/UI/UI.gd
@@ -5,6 +5,19 @@ extends CanvasLayer
 @onready var timeBar = $%TimeBar
 @onready var fireAnimation = $%FireAnimation
 
+@onready var CharacterButtons = [
+	$CharacterSelection/VBox/BottomBackground/MarginContainer/CharacterButtonsContainer/CharacterBg1/CharacterButton,
+	$CharacterSelection/VBox/BottomBackground/MarginContainer/CharacterButtonsContainer/CharacterBg2/CharacterButton,
+	$CharacterSelection/VBox/BottomBackground/MarginContainer/CharacterButtonsContainer/CharacterBg3/CharacterButton
+]
+
+@onready var ActionButtons = [
+	$ActionSelection/VBox/BottomBackground/MarginContainer/ActionSelectionButtons/ActionBg1/ActionButton,
+	$ActionSelection/VBox/BottomBackground/MarginContainer/ActionSelectionButtons/ActionBg2/ActionButton,
+	$ActionSelection/VBox/BottomBackground/MarginContainer/ActionSelectionButtons/ActionBg3/ActionButton,
+	$ActionSelection/VBox/BottomBackground/MarginContainer/ActionSelectionButtons/ActionBg4/ActionButton
+]
+
 var timerStarted: bool = false
 var timerEndTime: int = 0
 
@@ -14,46 +27,97 @@ var tweenTimeBarColor: Tween
 var tweenTimeBarSize: Tween
 var tweenFirePosition: Tween
 
+signal _on_choose_character(character: Globals.CharacterClass)
+signal _on_choose_action(action: Globals.CHARACTER_ACTION)
+
+
+func _ready() -> void:
+	Globals.state_started.connect(_on_state_started_received)
+	Globals.state_finished.connect(_on_state_finished_received)
+
 
 func _process(_delta) -> void:
 	timeBar.rotation_degrees = 180
 	timeBar.position += timeBar.size
 	fireAnimation.position.y -= 90 #fireAnimation.size.y doesnt work because blehhhhhhhhhhhhhhhhh
 	set_process(false)
+
+
+func _on_state_started_received(state: Globals.StateTurn) -> void:
+	match state:
+		Globals.StateTurn.CHOICE_CHARACTER:
+			hide_action_menu()
+			show_character_menu()
+		Globals.StateTurn.CHOICE_ACTION:
+			hide_character_menu()
+			show_action_menu()
+
+
+func _on_state_finished_received(state: Globals.StateTurn) -> void:
+	match state:
+		Globals.StateTurn.CHOICE_ACTION:
+			hide_character_menu()
+			hide_action_menu()
+
+
+func hide_character_menu():
+	for button in CharacterButtons:
+		button.disabled = true
 		
-
-signal _on_choose_character(character: Globals.CharacterClass)
-signal _on_choose_action(action: Globals.CHARACTER_ACTION)
-
-
-func switch_to_character_menu():
 	tweenCharacter = get_tree().create_tween()
-	tweenAction = get_tree().create_tween()
+	tweenCharacter.tween_property(characterSelectionMenu, "position", Vector2(0, 300), 1.5).set_trans(Tween.TRANS_BACK)
 	
-	# Move the menu from 2000 to 300 (See comment below)
+	# If the two Selection Menus overlap, the click doesn't propagate through
+	# We keep the tween at 300 for aesthetics, but then move it to 2000 to prevent overlap
+	await tweenCharacter.finished
+	characterSelectionMenu.position = Vector2(0, 2000)
+
+
+func show_character_menu():
+	tweenCharacter = get_tree().create_tween()
+	
+	# Move the menu from 2000 to 300
 	characterSelectionMenu.position = Vector2(0, 300)
 	tweenCharacter.tween_property(characterSelectionMenu, "position", Vector2(0, 0), 1.5).set_trans(Tween.TRANS_BACK)
+	
+	await tweenCharacter.finished
+	for button in CharacterButtons:
+		button.disabled = false
+	
+	
+func hide_action_menu():
+	for button in ActionButtons:
+		button.disabled = true
+		
+	tweenAction = get_tree().create_tween()
 	tweenAction.tween_property(actionSelectionMenu, "position", Vector2(0, 300), 1.5).set_trans(Tween.TRANS_BACK)
 	
 	# If the two Selection Menus overlap, the click doesn't propagate through
 	# We keep the tween at 300 for aesthetics, but then move it to 2000 to prevent overlap
 	await tweenAction.finished
 	actionSelectionMenu.position = Vector2(0, 2000)
+	
+	
+func show_action_menu():
+	tweenAction = get_tree().create_tween()
+	
+	# Move the menu from 2000 to 300
+	actionSelectionMenu.position = Vector2(0, 300)
+	tweenAction.tween_property(actionSelectionMenu, "position", Vector2(0, 0), 1.5).set_trans(Tween.TRANS_BACK)
+	
+	await tweenAction.finished
+	for button in ActionButtons:
+		button.disabled = false
+	
+	
+func switch_to_character_menu():
+	hide_action_menu()
+	show_character_menu()
 
 
 func switch_to_action_menu():
-	tweenCharacter = get_tree().create_tween()
-	tweenAction = get_tree().create_tween()
-	
-	# Move the menu from 2000 to 300 (See comment below)
-	actionSelectionMenu.position = Vector2(0, 300)
-	tweenCharacter.tween_property(characterSelectionMenu, "position", Vector2(0, 300), 1.5).set_trans(Tween.TRANS_BACK)
-	tweenAction.tween_property(actionSelectionMenu, "position", Vector2(0, 0), 1.5).set_trans(Tween.TRANS_BACK)
-	
-	# If the two Selection Menus overlap, the click doesn't propagate through
-	# We keep the tween at 300 for aesthetics, but then move it to 2000 to prevent overlap
-	await tweenCharacter.finished
-	characterSelectionMenu.position = Vector2(0, 2000)
+	hide_character_menu()
+	show_action_menu()
 
 
 func reset_timer():
@@ -91,6 +155,7 @@ func stop_timer():
 
 
 func _on_character_1_button_pressed() -> void:
+<<<<<<< HEAD
 	_on_choose_character.emit(Globals.CharacterClass.GUNNER)
 	switch_to_action_menu()
 	start_timer(10)
@@ -105,24 +170,30 @@ func _on_character_2_button_pressed() -> void:
 func _on_character_3_button_pressed() -> void:
 	_on_choose_character.emit(Globals.CharacterClass.TRAPPER)
 	switch_to_action_menu()
+=======
+	_on_choose_character.emit(1)
+
+
+func _on_character_2_button_pressed() -> void:
+	_on_choose_character.emit(2)
+
+
+func _on_character_3_button_pressed() -> void:
+	_on_choose_character.emit(3)
+>>>>>>> 18d52d0 (UI: connect the state machine from Helios to the UI buttons)
 
 
 func _on_action_1_button_pressed() -> void:
 	_on_choose_action.emit(1)
-	switch_to_character_menu()
-	stop_timer()
 
 
 func _on_action_2_button_pressed() -> void:
 	_on_choose_action.emit(2)
-	switch_to_character_menu()
 
 
 func _on_action_3_button_pressed() -> void:
 	_on_choose_action.emit(3)
-	switch_to_character_menu()
 
 
 func _on_action_4_button_pressed() -> void:
 	_on_choose_action.emit(4)
-	switch_to_character_menu()
