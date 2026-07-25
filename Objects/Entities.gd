acc5817745514aba79978be16087a13aea7fae6a
chloe <chloe200.durand@gmail.com>
2026-07-25 23:21:17 +0200

    Entities Move

--- Objects/Entities.gd
+++ Objects/Entities.gd
@@ -12,6 +12,63 @@ func init_entity(level: Level):
 func update_visual():
 	position = _level.getPostionAbsoluteFromCoordinates(caseCoords.x, caseCoords.y)
 
-func move(move_direction: Vector2i):
-	caseCoords += move_direction
-	update_visual()
+
+func move(move_direction: Vector2i): 
+	var stop :bool = false 
+	print("Move Global  : ", move_direction)
+	while move_direction.x!=0 && !stop :
+		if(move_direction.x >=0 ):
+			stop=_move(Vector2i(1,0))
+		
+			move_direction.x-=1
+		else : 
+			stop=_move(Vector2i(-1,0))
+			move_direction.x+=1
+	while move_direction.y!=0  && !stop :
+		if(move_direction.x >=0 ):
+			stop=_move(Vector2i(0,-1))
+		
+			move_direction.y-=1
+		else : 
+			stop=_move(Vector2i())
+			move_direction.y+=1
+
+#move after 
+func _move(move_direction: Vector2i)-> bool : 
+	print("Move  : ", move_direction)
+	var newCoords = caseCoords+ move_direction
+	print("caseCoords", caseCoords, "newCoords", newCoords)
+	var TRAP_REPULSE
+	var move_after=true
+	#TODO test merge enemies 
+	
+	#Outside Map 
+	if(newCoords.x >0 && newCoords.y >0 && newCoords.x<Globals.NUMBER_CELL_X && newCoords.y< Globals.NUMBER_CELL_Y ):
+		var case :Globals.TypeCase= _level.get_type_case_from_position(newCoords.x, newCoords.y)
+		# if possible
+		if(case != Globals.TypeCase.ICE && case != Globals.TypeCase.TREE && case != Globals.TypeCase.BUSH):
+			
+			caseCoords = newCoords
+			print("after compute ", caseCoords)
+			update_visual()
+			
+			if(case == Globals.TypeCase.TRAP_ICE ): 
+				#effet d' arrivée 
+				print("ICe trap  ")
+				put_ice_on_entity()
+			if( case ==Globals.TypeCase.TRAP_REPULSE):
+				print("TRAP_REPULSE trap  ")
+				move(Vector2i(2,0)) #2 case  on the right 
+
+		else :
+			
+			print("Not move block")
+			move_after=false
+	else :
+		print("Limit map ")
+		move_after=true
+	return move_after 
+
+
+func put_ice_on_entity():
+	pass
