6b1312a0c0d10e5d209b8924a1581158539c9301
neverhelios <elias.bina58@gmail.com>
2026-07-25 14:52:21 +0200

    feat: Assert state machine

--- Scenes/Game/Levels/level_portion.gd
+++ Scenes/Game/Levels/level_portion.gd
@@ -2,34 +2,48 @@ extends Node2D
 
 enum StateTurn  { CHOICE_CHARACTER, CHOICE_ACTION, ACTION_CHARACTER , DEPLACEMENT_ENEMIES}
 
-signal input_received
+signal state_finished(state: StateTurn)
 
-var state : StateTurn = StateTurn.CHOICE_CHARACTER
+var curr_state : StateTurn = StateTurn.CHOICE_CHARACTER
 var characterChosen : Globals.CharacterClass = Globals.CharacterClass.NONE;
 
+func _init() -> void:
+	state_finished.connect(_on_state_finished)
+
 func _process(_delta):
-	match state:
-		StateTurn.CHOICE_CHARACTER :
-			state=StateTurn.CHOICE_ACTION
-			#Choice Perso 
-		StateTurn.CHOICE_ACTION :
-			state=StateTurn.ACTION_CHARACTER
-			#set ACTION 
-		StateTurn.ACTION_CHARACTER :
-			state = StateTurn.DEPLACEMENT_ENEMIES
-			#emit ? 
-			#player.ExecuteAction(nextAction) # Add Action  
-			
-		StateTurn.DEPLACEMENT_ENEMIES :
-			state = StateTurn.CHOICE_CHARACTER 
-			
+	pass
+
 
 
 func _on_player_choose_character(character: Globals.CharacterClass):
 	print("Choose player %d " % character)
 	characterChosen = characterChosen
+	state_finished.emit(StateTurn.CHOICE_CHARACTER)
 	
 
 func _on_player_choose_action(action: Globals.CHARACTER_ACTION):
 	print("Choose action %d " % action)
 	#Do action or ask input
+	state_finished.emit(StateTurn.CHOICE_ACTION)
+
+	
+func _on_state_finished(state: StateTurn):
+	print("State finished: %s" % state)
+	assert(state == curr_state)
+	
+	match state:
+		StateTurn.CHOICE_CHARACTER :
+			curr_state=StateTurn.CHOICE_ACTION
+		StateTurn.CHOICE_ACTION :
+			if(1):
+				curr_state=StateTurn.ACTION_CHARACTER
+			else:
+				curr_state = StateTurn.DEPLACEMENT_ENEMIES
+			#set ACTION 
+		StateTurn.ACTION_CHARACTER :
+			curr_state = StateTurn.DEPLACEMENT_ENEMIES
+			#emit ? 
+			#player.ExecuteAction(nextAction) # Add Action  
+			
+		StateTurn.DEPLACEMENT_ENEMIES :
+			curr_state = StateTurn.CHOICE_CHARACTER 
