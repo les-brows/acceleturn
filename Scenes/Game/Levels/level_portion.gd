afa20bdc9108e4724d092c2558eb0fbacd500ac4
neverhelios <elias.bina58@gmail.com>
2026-07-25 14:39:18 +0200

    feat: Add state turn

--- Scenes/Game/Levels/level_portion.gd
+++ Scenes/Game/Levels/level_portion.gd
@@ -1,7 +1,35 @@
 extends Node2D
 
-func _on_player_choose_character(playerIndex: int):
-	print("Choose player %d " % playerIndex)
+enum StateTurn  { CHOICE_CHARACTER, CHOICE_ACTION, ACTION_CHARACTER , DEPLACEMENT_ENEMIES}
+
+signal input_received
+
+var state : StateTurn = StateTurn.CHOICE_CHARACTER
+var characterChosen : Globals.CharacterClass = Globals.CharacterClass.NONE;
+
+func _process(_delta):
+	match state:
+		StateTurn.CHOICE_CHARACTER :
+			state=StateTurn.CHOICE_ACTION
+			#Choice Perso 
+		StateTurn.CHOICE_ACTION :
+			state=StateTurn.ACTION_CHARACTER
+			#set ACTION 
+		StateTurn.ACTION_CHARACTER :
+			state = StateTurn.DEPLACEMENT_ENEMIES
+			#emit ? 
+			#player.ExecuteAction(nextAction) # Add Action  
+			
+		StateTurn.DEPLACEMENT_ENEMIES :
+			state = StateTurn.CHOICE_CHARACTER 
+			
+
+
+func _on_player_choose_character(character: Globals.CharacterClass):
+	print("Choose player %d " % character)
+	characterChosen = characterChosen
+	
 
 func _on_player_choose_action(action: Globals.CHARACTER_ACTION):
 	print("Choose action %d " % action)
+	#Do action or ask input
