619ccc43de0d4c57ee55c59ca51f60a76c91dbcb
neverhelios <elias.bina58@gmail.com>
2026-07-25 15:43:03 +0200

    feat: Add enemy manager + Update state machine

---
+++ Scenes/Game/Levels/level.gd
@@ -0,0 +1,50 @@
+extends Node2D
+
+@onready var enemyTurnManager: EnemyTurnManager = $EnemyTurnManager
+
+var curr_state : Globals.StateTurn = Globals.StateTurn.CHOICE_CHARACTER
+var characterChosen : Globals.CharacterClass = Globals.CharacterClass.NONE;
+
+func _init() -> void:
+	Globals.state_finished.connect(_on_state_finished)
+
+func _process(_delta):
+	pass
+
+
+
+func _on_player_choose_character(character: Globals.CharacterClass):
+	print("Choose player %d " % character)
+	characterChosen = characterChosen
+	Globals.state_finished.emit(Globals.StateTurn.CHOICE_CHARACTER)
+	
+
+func _on_player_choose_action(action: Globals.CHARACTER_ACTION):
+	print("Choose action %d " % action)
+	#Do action or ask input
+	Globals.state_finished.emit(Globals.StateTurn.CHOICE_ACTION)
+
+	
+func _on_state_finished(state: Globals.StateTurn):
+	print("State finished: %s" % state)
+	assert(state == curr_state)
+	
+	match state:
+		Globals.StateTurn.CHOICE_CHARACTER :
+			curr_state=Globals.StateTurn.CHOICE_ACTION
+		Globals.StateTurn.CHOICE_ACTION :
+			if(0):
+				curr_state=Globals.StateTurn.ACTION_CHARACTER
+				#set ACTION 
+			else:
+				curr_state = Globals.StateTurn.DEPLACEMENT_ENEMIES
+				
+		Globals.StateTurn.ACTION_CHARACTER :
+			curr_state = Globals.StateTurn.DEPLACEMENT_ENEMIES
+			#emit ? 
+			#player.ExecuteAction(nextAction) # Add Action  
+		Globals.StateTurn.DEPLACEMENT_ENEMIES :
+			curr_state = Globals.StateTurn.CHOICE_CHARACTER 
+	
+	Globals.state_started.emit(curr_state)
+	print("Send state %d" % curr_state)
