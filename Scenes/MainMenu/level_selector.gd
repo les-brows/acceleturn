964d6d87f6e1c95e36c89735abedd1d1ccc012a6
Nebulea <github@nebulea.dev>
2026-07-26 17:25:08 +0200

    feat: make main menu and level selector clickable

--- Scenes/MainMenu/level_selector.gd
+++ Scenes/MainMenu/level_selector.gd
@@ -95,11 +95,16 @@ func _process(_delta: float) -> void:
 		handle_level_selection(current_selection)
 		
 func set_selected_level(previous_level: int, level: int) -> void:
-	var previous_selector = selectors[previous_level]
-	var selector = selectors[level]
+	unset_selected_level(previous_level)
 	
-	previous_selector.get_node("Selected").visible = false
+	var selector = selectors[level]
 	selector.get_node("Selected").visible = true
+
+
+func unset_selected_level(level: int) -> void:
+	var selector = selectors[level]
+	selector.get_node("Selected").visible = false
+
 
 func handle_level_selection(level: int) -> void:
 	var level_node
@@ -142,8 +147,154 @@ func hide_unlocked_levels(max_level: int) -> void:
 	for i in range(0, NUM_OF_LEVELS):
 		var selector = selectors[i]
 		if ( i >= max_level ):
-			selector.get_node("Locked").visible = true
 			selector.get_node("Option").text = ""
+			selector.get_node("Locked").visible = true
+			selector.get_node("Button").disabled = true
 		else :
 			selector.get_node("Option").text = "%d" % (i+1)
 			selector.get_node("Locked").visible = false
+			selector.get_node("Button").disabled = false
+
+
+func _on_button_1_mouse_entered() -> void:
+	set_selected_level(previous_selection, 0)
+
+
+func _on_button_1_mouse_exited() -> void:
+	unset_selected_level(0)
+
+
+func _on_button_1_pressed() -> void:
+	handle_level_selection(0)
+
+
+func _on_button_2_mouse_entered() -> void:
+	set_selected_level(previous_selection, 1)
+
+
+func _on_button_2_mouse_exited() -> void:
+	unset_selected_level(1)
+
+
+func _on_button_2_pressed() -> void:
+	handle_level_selection(1)
+
+
+func _on_button_3_mouse_entered() -> void:
+	set_selected_level(previous_selection, 2)
+
+
+func _on_button_3_mouse_exited() -> void:
+	unset_selected_level(2)
+
+
+func _on_button_3_pressed() -> void:
+	handle_level_selection(2)
+
+
+func _on_button_4_mouse_entered() -> void:
+	set_selected_level(previous_selection, 3)
+
+
+func _on_button_4_mouse_exited() -> void:
+	unset_selected_level(3)
+
+
+func _on_button_4_pressed() -> void:
+	handle_level_selection(3)
+
+
+func _on_button_5_mouse_entered() -> void:
+	set_selected_level(previous_selection, 4)
+
+
+func _on_button_5_mouse_exited() -> void:
+	unset_selected_level(4)
+
+
+func _on_button_5_pressed() -> void:
+	handle_level_selection(4)
+
+
+func _on_button_6_mouse_entered() -> void:
+	set_selected_level(previous_selection, 5)
+
+
+func _on_button_6_mouse_exited() -> void:
+	unset_selected_level(5)
+
+
+func _on_button_6_pressed() -> void:
+	handle_level_selection(5)
+
+
+func _on_button_7_mouse_entered() -> void:
+	set_selected_level(previous_selection, 6)
+
+
+func _on_button_7_mouse_exited() -> void:
+	unset_selected_level(6)
+
+
+func _on_button_7_pressed() -> void:
+	handle_level_selection(6)
+
+
+func _on_button_8_mouse_entered() -> void:
+	set_selected_level(previous_selection, 7)
+
+
+func _on_button_8_mouse_exited() -> void:
+	unset_selected_level(7)
+
+
+func _on_button_8_pressed() -> void:
+	handle_level_selection(7)
+
+
+func _on_button_9_mouse_entered() -> void:
+	set_selected_level(previous_selection, 8)
+
+
+func _on_button_9_mouse_exited() -> void:
+	unset_selected_level(8)
+
+
+func _on_button_9_pressed() -> void:
+	handle_level_selection(8)
+
+
+func _on_button_10_mouse_entered() -> void:
+	set_selected_level(previous_selection, 9)
+
+
+func _on_button_10_mouse_exited() -> void:
+	unset_selected_level(9)
+
+
+func _on_button_10_pressed() -> void:
+	handle_level_selection(9)
+
+
+func _on_button_11_mouse_entered() -> void:
+	set_selected_level(previous_selection, 10)
+
+
+func _on_button_11_mouse_exited() -> void:
+	unset_selected_level(10)
+
+
+func _on_button_11_pressed() -> void:
+	handle_level_selection(10)
+
+
+func _on_button_12_mouse_entered() -> void:
+	set_selected_level(previous_selection, 11)
+
+
+func _on_button_12_mouse_exited() -> void:
+	unset_selected_level(11)
+
+
+func _on_button_12_pressed() -> void:
+	handle_level_selection(11)
