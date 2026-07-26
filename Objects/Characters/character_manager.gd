007eb01fea7aa744bd2ef899b390950eaeaa966c
neverhelios <elias.bina58@gmail.com>
2026-07-26 14:28:22 +0200

    feat: Move all enemis for backtracking

--- Objects/Characters/character_manager.gd
+++ Objects/Characters/character_manager.gd
@@ -162,3 +162,10 @@ func getEntityAtPos(tile : Vector2i) -> Entity:
 			if(child.caseCoords == tile):
 				return	child
 	return null
+
+func getAllCharacters() -> Array[Entity]:
+	var all_enemies : Array[Entity] = []
+	for child in get_children():
+		if child is Entity:
+			all_enemies.append(child)
+	return all_enemies
