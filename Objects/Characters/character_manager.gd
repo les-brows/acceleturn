e6350402095d69af20feacf647a121fc188dfd68
neverhelios <elias.bina58@gmail.com>
2026-07-26 13:36:49 +0200

    feat: Added restrictions on abilities

--- Objects/Characters/character_manager.gd
+++ Objects/Characters/character_manager.gd
@@ -10,6 +10,7 @@ func _ready() -> void:
 func _on_state_started(state: Globals.StateTurn):
 	if(state == Globals.StateTurn.ACTION_CHARACTER):
 		process_character_turn()
+		await get_tree().create_timer(0.2).timeout
 		Globals.state_finished.emit(Globals.StateTurn.ACTION_CHARACTER)
 
 func init_characters_positions():
@@ -49,13 +50,27 @@ func set_current_tile(tile: Vector2i):
 
 func can_choose_tile(tile: Vector2i) -> bool:
 	var typeCase: Globals.TypeCase = get_parent().get_type_case_from_position(tile.x, tile.y)
+	var canChoose: bool = true
 	print(typeCase)
-	# Only entities
 	
+	# Only entities
 	if(Globals.curr_character == Globals.CharacterClass.GUNNER && Globals.curr_action == Globals.CharacterAction.ACTION1):
-		if(typeCase == Globals.TypeCase.ENEMIES or typeCase == Globals.TypeCase.CHARACTER):
-			return true
-		return false
+		if(!(typeCase == Globals.TypeCase.ENEMIES or typeCase == Globals.TypeCase.CHARACTER)):
+			return false
+			
+	
+	# Only horizontal
+	if(Globals.curr_character == Globals.CharacterClass.GUNNER && Globals.curr_action == Globals.CharacterAction.ACTION1):
+		# Check for the gunner
+		if(tile.y != get_gunner().caseCoords.y):
+			return false
+	
+	# Only vertical
+	if(Globals.curr_character == Globals.CharacterClass.GUNNER && Globals.curr_action == Globals.CharacterAction.ACTION3):
+		# Check for the gunner
+		if(tile.x != get_gunner().caseCoords.x):
+			return false
+
 	return true
 
 func action_needs_target() -> bool:
@@ -101,19 +116,31 @@ func process_character_turn():
 		return;
 
 	if(Globals.curr_character == Globals.CharacterClass.GUNNER):
-		for child in get_children():
-			if child is Gunner:
-				child.act(Globals.curr_action, curr_tile)
+		get_gunner().act(Globals.curr_action, curr_tile)
 
 	if(Globals.curr_character == Globals.CharacterClass.MAGE):
-		for child in get_children():
-			if child is Mage:
-				child.act(Globals.curr_action, curr_tile)
+		get_mage().act(Globals.curr_action, curr_tile)
 
 	if(Globals.curr_character == Globals.CharacterClass.TRAPPER):
-		for child in get_children():
-			if child is Trapper:
-				child.act(Globals.curr_action, curr_tile)
+		get_trapper().act(Globals.curr_action, curr_tile)
+				
+func get_gunner() -> Entity:
+	for child in get_children():
+		if child is Gunner:
+			return child
+	return null
+
+func get_mage() -> Entity:
+	for child in get_children():
+		if child is Mage:
+			return child
+	return null
+
+func get_trapper() -> Entity:
+	for child in get_children():
+		if child is Trapper:
+			return child
+	return null
 				
 func getEntityAtPos(tile : Vector2i) -> Entity:
 	for child in get_children():
